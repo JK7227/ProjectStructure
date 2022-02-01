3928075d9018bbc4f393acda677dfd53efb579d2
krishna <joesmash1998@gmail.com>
2022-02-01 19:36:54 +0530

    Project

---
+++ src/test/java/org/project/A.java
@@ -0,0 +1,11 @@
+package org.project;
+
+public class A {
+	public static void main(String[] args) {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		
+	}
+
+}
